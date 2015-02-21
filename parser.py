4dd9a06ce5a855f795848b6c8a16a4fe007d015f
Tommy Phan <tommyphan8@csu.fullerton.edu>
2015-02-20 20:53:10 -0800

    finished function that finds largest word

--- parser.py
+++ parser.py
@@ -6,8 +6,11 @@ letterCount = {'A': 0, 'B': 0, 'C': 0, 'D': 0, 'E': 0, 'F': 0, 'G': 0, 'H': 0, '
 
 wordCount = {"THE": 0, "WHEN": 0}
 
-message = "Mark Antony's was by Caesar. He chid the sisters"
+#message = "Mark Antony's was by Caesar. He chid the sisters"
+message = "asd Hello Hello asdfg wqert asdf"
 message = message.replace("-", "")
+
+largestWord = []
 
 #counts the the letter frequency 
 #@message - string that the function will count
@@ -17,6 +20,7 @@ def getLetterCount(message, letterCount):
 		if letter in LETTERS:
 			letterCount[letter] += 1
 	print(letterCount)		
+
 
 #counts the word frequency
 #@message - string that the function will parse and count
@@ -30,6 +34,21 @@ def getWordCount(message, wordCount):
 			wordCount[word] += 1
 	print (wordCount)
 
+
+def getBiggestWord(message, largestWord):
+	wordArray = message.upper().split()
+	for word in wordArray:
+		if not largestWord:
+			largestWord.append(word)
+		elif len(word) == len(largestWord[0]) and word != largestWord[0]:
+			largestWord.append(word)
+		elif len(word) > len(largestWord[0]):
+			del largestWord[:]
+			largestWord.append(word)
+	print largestWord
+			
+
 print message
+getBiggestWord(message, largestWord)
 getWordCount(message, wordCount)
 getLetterCount(message, letterCount)
