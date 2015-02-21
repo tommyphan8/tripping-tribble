7af3a454baf1976c20dad508347432e061c01081
Tommy Phan <tommyphan8@csu.fullerton.edu>
2015-02-20 22:15:25 -0800

    Finished the requirements, need to implement the bar graph for frequency of letter

--- parser.py
+++ parser.py
@@ -1,30 +1,35 @@
+import sys
+import os
+import matplotlib.pyplot as plt
+import numpy
+
 LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
+#set that stores frequency of letters"
 letterCount = {'A': 0, 'B': 0, 'C': 0, 'D': 0, 'E': 0, 'F': 0, 'G': 0, 'H': 0, 'I': 0,
 'J': 0, 'K': 0, 'L': 0, 'M': 0, 'N': 0, 'O': 0, 'P': 0, 'Q': 0, 'R': 0, 'S': 0, 'T': 0, 'U': 0,
 'V': 0, 'W': 0, 'X': 0, 'Y': 0, 'Z': 0}
 
+
+#set that stores frequency of words: "the" and "when"
 wordCount = {"THE": 0, "WHEN": 0}
 
-#message = "Mark Antony's was by Caesar. He chid the sisters"
-message = "asd Hello Hello asdfg wqert asdf"
-message = message.replace("-", "")
-
+#list that stores largest word
 largestWord = []
 
 #counts the the letter frequency 
 #@message - string that the function will count
-#@letterCount - array that stores the frequency of each letter
+#@letterCount - set that stores the frequency of each letter
 def getLetterCount(message, letterCount):
 	for letter in message.upper():
 		if letter in LETTERS:
 			letterCount[letter] += 1
-	print(letterCount)		
+	#print(letterCount)		
 
 
 #counts the word frequency
 #@message - string that the function will parse and count
-#@
+#@wordCount - set that stores frequency of words
 def getWordCount(message, wordCount):
 	wordArray = message.upper().split()
 	for word in wordArray:
@@ -32,9 +37,9 @@ def getWordCount(message, wordCount):
 			wordCount[word] +=1
 		elif word == "WHEN":
 			wordCount[word] += 1
-	print (wordCount)
+	#print (wordCount)
 
-
+#checks if 
 def getBiggestWord(message, largestWord):
 	wordArray = message.upper().split()
 	for word in wordArray:
@@ -45,10 +50,36 @@ def getBiggestWord(message, largestWord):
 		elif len(word) > len(largestWord[0]):
 			del largestWord[:]
 			largestWord.append(word)
-	print largestWord
-			
+	#print largestWord
 
-print message
-getBiggestWord(message, largestWord)
-getWordCount(message, wordCount)
-getLetterCount(message, letterCount)
+def main(file, LETTERS, letterCount, wordCount, largestWord):
+
+	#Check if file exists	
+	if not os.path.exists(file):
+		print("Error: " + file + " does not exists")
+		exit(1)
+	
+	#Open file for reading
+	fi = open(file, "r")
+
+	for line in fi:
+		message = line		
+		message = message.replace("-", "")
+		getBiggestWord(message, largestWord)
+		getWordCount(message, wordCount)
+		getLetterCount(message, letterCount)
+	fi.close()
+						
+	print largestWord
+	print wordCount
+	print letterCount
+	print("Completed Successfully")
+
+
+if __name__ == "__main__":
+    # Check number of command line arguments and call CommentOut function
+    if len(sys.argv) == 2:
+	main(sys.argv[1], LETTERS, letterCount, wordCount, largestWord)       
+        exit(0)
+    print("Usage: " + sys.argv[0] + " <file>")
+    exit(1)
